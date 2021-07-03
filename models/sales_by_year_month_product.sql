1694ea3c62b5298e5d231b393f5c89c8c11e9aec
Elliot Wargo <elliot.wargo@gmail.com>
2021-07-03 13:42:20 -0700

    Remove whitespace

--- models/sales_by_year_month_product.sql
+++ models/sales_by_year_month_product.sql
@@ -1,12 +1,12 @@
 with sales as (select * from `bigquery-public-data.iowa_liquor_sales.sales`),
 
 totals as (
-    select 
+    select
         extract(year from date) as year_of_purchase,
         extract(month from date) as month_of_purchase,
         item_description,
         round(sum(volume_sold_gallons), 2) as gallons_purchased
-    from 
+    from
     group by 1, 2, 3
 )
 
